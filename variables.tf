6a83d3ed2355c9bad698c67f02d0882e6ef24d59
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-17 22:48:57 +0530

    actions

--- variables.tf
+++ variables.tf
@@ -1,5 +1,19 @@
+# project/project_name can be used as you wish(Not necessarily it should be same but it gives the exact default value provided).
+variable "project" {
+  default   = "roboshop"
+}
+
+variable "environment" {
+  default   = "dev"
+}
+
 variable "component" {
   type      = string
+}
+
+variable "app_version" {
+  type      = string
+  default   = "v3"
 }
 
 variable "health_check_path" {
@@ -13,4 +27,8 @@ variable "port_number" {
 
 variable "rule_priority" {
   default   = 10
+}
+
+variable "domain_name" {
+  default   = "devopsdaws.online"
 }
